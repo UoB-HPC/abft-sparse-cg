d8fe862492f9fadf54aa78f8ee23cefe0e320bd3
James Price <j.price@bristol.ac.uk>
2016-05-01 18:45:55 +0100

    Remove TODOs from plain spmv.c file

--- spmv.c
+++ spmv.c
@@ -3,17 +3,7 @@
 // Initialize ECC for a sparse matrix
 void init_matrix_ecc(sparse_matrix M)
 {
-  // Add ECC protection to matrix elements
-  for (unsigned i = 0; i < M.nnz; i++)
-  {
-    matrix_entry element = M.elements[i];
-
-    // TODO: Generate parity bits and store in high order column bits
-    // Use the ecc_compute_col8 function to generate the parity bits
-    // and then add them to the column index with the | operator (bitwise OR)
-
-    M.elements[i] = element;
-  }
+  // Nothing to do
 }
 
 // Sparse matrix vector product
@@ -30,15 +20,6 @@ void spmv(sparse_matrix matrix, double *vector, double *result, unsigned N)
   {
     // Load non-zero element
     matrix_entry element = matrix.elements[i];
-
-    // TODO: Check ECC here
-    // Use the ecc_compute_col8 function to check all of the parity bits
-    // If there was an error use the ecc_correct_col8 function to
-    // correct it
-
-    // TODO: Mask out the parity bits from the high order column bits so that
-    // we can use the column index
-    // Use the & operator (bitwise AND) with an approriate mask
 
     // Multiply element value by the corresponding vector value
     // and accumulate into result vector
