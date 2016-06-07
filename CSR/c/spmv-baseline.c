62279368af392b2456f668e84650ea955f75bfb0
James Price <j.price@bristol.ac.uk>
2016-06-07 20:34:45 +0100

    Remove comments from CSR baseline file

--- CSR/c/spmv-baseline.c
+++ CSR/c/spmv-baseline.c
@@ -11,10 +11,6 @@ void init_matrix_ecc(sparse_matrix M)
 // The matrix and vector dimensions are `N`
 void spmv(sparse_matrix matrix, double *vector, double *result, unsigned N)
 {
-  // Initialize result vector to zero
-  //for (unsigned i = 0; i < N; i++)
-  //  result[i] = 0.0;
-
   for (unsigned row = 0; row < N; row++)
   {
     double tmp = 0.0;
@@ -29,15 +25,4 @@ void spmv(sparse_matrix matrix, double *vector, double *result, unsigned N)
 
     result[row] = tmp;
   }
-
-  // // Loop over non-zeros in matrix
-  // for (unsigned i = 0; i < matrix.nnz; i++)
-  // {
-  //   // Load non-zero element
-  //   matrix_entry element = matrix.elements[i];
-  //
-  //   // Multiply element value by the corresponding vector value
-  //   // and accumulate into result vector
-  //   result[element.col] += element.value * vector[element.row];
-  // }
 }
